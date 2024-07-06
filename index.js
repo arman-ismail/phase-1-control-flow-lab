ed50303c76fecbb70d12ddbc19758895252fa1a6
arman ismail <armanismail544@gmail.com>
2024-07-06 17:50:35 +0300

    innitial commit

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 function scuberGreetingForFeet(feet) {
-  let response;
+  // Write your code here!
   if (feet <= 400) {
-    response = 'This one is on me!';
+    return "This one is on me!";
+  } else if (feet > 400 && feet <= 2000) {
+    return "That will be twenty bucks.";
   } else if (feet > 2000 && feet <= 2500) {
-    response = 'I will gladly take your thirty bucks.';
-  } else if (feet > 2500) {
-    response = 'No can do.';
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
   }
-  return response;
 }
 
 function ternaryCheckCity(city) {
-  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
 function switchOnCharmFromTip(tip) {
+  // Write your code here!
   switch (tip) {
     case "generous":
       return "Thank you so much.";
